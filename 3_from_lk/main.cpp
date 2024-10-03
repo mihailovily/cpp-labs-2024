b6d05f0eda16e73af52ffc97ead7925f94eb5a35
Ilya <mihailovily@tuta.io>
2024-10-03 15:07:37 +0300

    changes

--- 3_from_lk/main.cpp
+++ 3_from_lk/main.cpp
@@ -5,47 +5,53 @@ using namespace std;
 
 int main()
 {
-    float y;
-    float x = 2.8;
-    int N = 10;
-    float sum = 0;
-    char variant;
-    int i = 1;
-    cin >> variant;
-    switch (variant)
+    char variant = '1';
+    while (variant != '0')
     {
-    case '1':
-        /* FOR */
-        for (i = 1; i <= N; i++)
+        float y = 0;
+        float x = 2.8;
+        int N = 10;
+        float sum = 0;
+        int i = 1;
+        y = 0;
+        cin >> variant;
+        switch (variant)
         {
-            sum = sum + (cos(i * x) + (cos(x) / cos(i)));
+        case '1':
+            /* FOR */
+            for (i = 1; i <= N; i++)
+            {
+                sum = sum + (cos(i * x) + (cos(x) / cos(i)));
+            }
+            y = 2.8 * x * sum;
+            cout << y << endl;
+            break;
+        case '2':
+            /* WHILE*/
+            while (i <= N)
+            {
+                sum = sum + (cos(i * x) + (cos(x) / cos(i)));
+                i++;
+            }
+            y = 2.8 * x * sum;
+            cout << y << endl;
+            break;
+        case '3':
+            /*DO WHILE*/
+            do
+            {
+                sum = sum + (cos(i * x) + (cos(x) / cos(i)));
+                i++;
+            } while (i <= N);
+
+            y = 2.8 * x * sum;
+            cout << y << endl;
+            break;
+        case '0':
+            break;
+        default:
+            cout << "НЕВЕРНЫЙ СИМВОЛ" << endl;
+            break;
         }
-        y = 2.8 * x * sum;
-        break;
-    case '2':
-        /* WHILE*/
-        while (i <= N)
-        {
-            sum = sum + (cos(i * x) + (cos(x) / cos(i)));
-            i++;
-        }
-        y = 2.8 * x * sum;
-        break;
-    case '3':
-        /*DO WHILE*/
-        do
-        {
-            sum = sum + (cos(i * x) + (cos(x) / cos(i)));
-            i++;
-        } while (i <= N);
-
-        y = 2.8 * x * sum;
-        break;
-
-    default:
-
-        cout << "НЕВЕРНЫЙ СИМВОЛ" << endl;
-        break;
     }
-    cout << y << endl;
 }
